126285fc839415ddc43cf7ca563f1da3d4f9c7b7
kwiats <pawuskwiatkowski@gmail.com>
2023-02-24 21:24:23 +0100

    created model for task

--- Backend/flask-app/app.py
+++ Backend/flask-app/app.py
@@ -1,16 +1,34 @@
 from flask import Flask
-from db import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy()
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-app.config["DEBUG"] = True
+with app.app_context():
+    db.create_all()
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(255), nullable=True)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Task {self.title} - status {self.done}>"
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    task = Task(title="first task", desc="zrob to i to")
+    return f"<p>Hello, World!</p> {task.title} {task.desc}"
 
 
 if __name__ == "__main__":
-    flask.run(debug=True)
+    app.run(debug=True)
